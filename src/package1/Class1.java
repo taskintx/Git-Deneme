93ec524f0b91455d8b9bde39b0b575100cdadc4b
taskintx <79757579+taskintx@users.noreply.github.com>
2021-02-28 10:07:43 -0600

    Update Class1.java
    Here is my additional commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,8 +3,9 @@ package package1;
 public class Class1 {
 
 	public static void main(String[] args) {
-		//Hello World!
-		// GitHub first seasison
+		// Hello World!
+		// Welcome to GitHub Sessions
+		// Thank you for joining
 
 	}
 
